5f37b2349e78902cfe04affa471a187f8078df9b
PH68613 <quan291027@gmail.com>
2025-11-29 10:54:36 +0700

    Code tren lop

--- 25FA-BL2-COM108-WD21308-PH68613/25FA-BL2-COM108-WD21308-PH68613/Program.c
+++ 25FA-BL2-COM108-WD21308-PH68613/25FA-BL2-COM108-WD21308-PH68613/Program.c
@@ -14,7 +14,7 @@ int main()
 	int mang[1000];
 	for ( i = 0; i < n; i++)
 	{
-		printf("nhap gia tri cho phan tu thu [%d]:", i +1 );
+		printf("nhap gia tri cho phan tu thu [%d]:", i + 1 );
 		scanf("%d", &mang[i]);
 	}
 	for ( i = 0; i < n; i++)
---
+++ codetrenlop/codetrenlop/Program.c
@@ -0,0 +1,93 @@
+// CConsoleApplication.c : file nay chua 'main' function. 
+// Chuong trinh phan mem Bat dau thuc thi & Ket thuc o day.
+
+#include <stdio.h>
+void kiemTraSoNguyen()
+{
+	printf("Kiem Tra So Nguyen");
+}
+
+void uocChungBoiChung()
+{
+	printf("Uoc Chung Boi Chung");
+}
+
+void lapChucNang(int chonChucNang)
+{
+	int tiepTuc = 1;
+	while (tiepTuc == 1)
+	{
+		switch (chonChucNang)
+		{
+		case 1:
+			kiemTraSoNguyen();
+			break;
+		case 2:
+			uocChungBoiChung();
+			break;
+		case 3:
+			// goi TEN ham CN3
+			break;
+		case 0:
+			return;
+		default:
+			printf("Hay chon lai [0-3]");
+			break;
+		}
+
+		printf("\n");
+		printf("Tiep tuc CN nay? [1=Co | 0=Khong]: ");
+		scanf("%d", &tiepTuc);
+		system("cls");
+	}
+}
+void sapXepMang1Chieu() {
+	int intergerArray[100];
+	int length;
+	int tmp;
+	printf("Nhap so luong phan tu mang:");
+	scanf("%d", &length);
+	printf("Nhap du lieu mang %d phan tu\n", length);
+	for (int i = 0; i < length; i++)
+	{
+		printf("mang[%d]=", i);
+		scanf("%d", &intergerArray[i]);
+	}
+	printf("Sap xep mang theo thu tu tang dan %d phan tu\n", length);
+	for (int i = 0; i < length - 1; i++)
+	{
+		if (intergerArray[i] > intergerArray[i + 1]) {
+			tmp = intergerArray[i];
+			intergerArray[i] = intergerArray[i + 1];
+			intergerArray[i + 1] = tmp;
+			i = -1;
+		}
+	}
+	printf("Xuat mang du lieu %d phan tu  Giam dan\n", length);
+}
+
+
+
+int main()
+{
+	int chonChucNang;
+	do
+	{
+		printf("Menu");
+		printf("\n");
+		printf("1. Kiem Tra So Nguyen");
+		printf("\n");
+		printf("2. TEN chuc nang 2");
+		printf("\n");
+		printf("3. TEN chuc nang 3");
+		printf("\n");
+		printf("Hay chon CN [0-3]: ");
+		scanf("%d", &chonChucNang);
+		lapChucNang(chonChucNang);
+	} while (chonChucNang != 0);
+}
+
+
+
+
+// Debug/Run chuong trinh: bam "F5" hoac "Debug > Start Debugging" tren menu
